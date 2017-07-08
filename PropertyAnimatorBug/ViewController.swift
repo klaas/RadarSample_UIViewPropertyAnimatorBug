975521c09e216b304be937b807e2d5c15d04aa50
Klaas <klaas@users.noreply.github.com>
2017-07-08 13:11:24 +0200

    Xcode 8.3 project template

---
+++ PropertyAnimatorBug/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PropertyAnimatorBug
+//
+//  Created by Klaas on 07.07.17.
+//  Copyright © 2017 Park Bench. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
+
